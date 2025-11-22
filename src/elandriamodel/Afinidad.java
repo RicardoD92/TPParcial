11cc60284e4b25937d79be6d819d2613eb2a6c90
RicardoD92 <63320997+RicardoD92@users.noreply.github.com>
2025-11-22 19:13:15 -0300

    creacion carpeta test

--- src/elandriamodel/Afinidad.java
+++ src/elandriamodel/Afinidad.java
@@ -2,6 +2,8 @@ package elandriamodel;
 
 public class Afinidad {
 
+	public static final String FUEGO = null;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/elandriamodel/Criatura.java
+++ src/elandriamodel/Criatura.java
@@ -2,4 +2,6 @@ package elandriamodel;
 
 public class Criatura {
 
+
+
 }
